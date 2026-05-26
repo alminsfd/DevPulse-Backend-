6cfc52be57a19acfe747438f657c3fb1999114c2
alminsfd <alaminhossaintanvir42@gmail.com>
2026-05-26 06:18:37 +0600

    set application and server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import type { Application, Request, Response } from "express";
 import express from 'express'
+import CookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Application = express()
 app.get('/', (req: Request, res: Response) => {
@@ -8,6 +10,19 @@ app.get('/', (req: Request, res: Response) => {
           message: "A collaborative platform for software teams to report bugs, suggest features, and coordinate resolutions"
      })
 })
+
+//build in middleware
+app.use(CookieParser());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+     cors({
+          origin: "http://localhost:5000",
+     }),
+);
+
+
 
 
 export default app
